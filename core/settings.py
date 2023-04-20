d53a3bcb0698a25c58d0054a6f220bda43da8360
Omurbek Dulatov <oma.dulatov@gmail.com>
2023-04-20 20:02:26 +0600

    Added static files

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+import os
 from os import getenv
 from pathlib import Path
 
@@ -35,7 +36,7 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -86,6 +87,19 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'templates', 'assets'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
